19d4e5d725311ed7b31260ff5250be7246fa8aa1
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-03-04 16:03:15 +0530

    JavaScripts

---
+++ Work/work1/scripts.js
@@ -0,0 +1,72 @@
+// --A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.     --//
+// --Ask user for their salary and year of service and print the net bonus amount.                          --//
+
+                    // let salary = 10000;
+                    // let year = 8;
+                    // let bonus = 0;
+
+                    // if (year > 5) {
+                    //     bonus = salary * 0.05;
+                    // }
+
+                    // let total = salary + bonus;
+                    // console.log(total);
+
+
+// --Write a program to accept a number from 1 to 7 and display the name of the day like 1 for Sunday , 2 for Monday and so on. --//
+
+                // let dayNumber = parseInt(prompt("Enter a number between 1 and 7: "), 10);
+
+                // switch (dayNumber) {
+                //     case 1:
+                //         console.log("Sunday");
+                //         break;
+                //     case 2:
+                //         console.log("Monday");
+                //         break;
+                //     case 3:
+                //         console.log("Tuesday");
+                //         break;
+                //     case 4:
+                //         console.log("Wednesday");
+                //         break;
+                //     case 5:
+                //         console.log("Thursday");
+                //         break;
+                //     case 6:
+                //         console.log("Friday");
+                //         break;
+                //     case 7:
+                //         console.log("Saturday");
+                //         break;
+                //     default:
+                //         console.log("Invalid input! Please enter a number between 1 and 7.");
+                // }
+
+//-- Q. Accept any city from the user and display monument of that city. --//
+//--                   City                                 Monument     --//
+//--                   Delhi                               Red Fort      --//
+//--                   Agra                                Taj Mahal     --//
+//--                   Jaipur                              Jal Mahal     --//
+
+                // let city = prompt("Enter the name of a city (Delhi, Agra, Jaipur): ").toLowerCase();
+
+                // switch (city) {
+                //     case "delhi":
+                //         console.log("The monument in Delhi is the Red Fort.");
+                //         break;
+                //     case "agra":
+                //         console.log("The monument in Agra is the Taj Mahal.");
+                //         break;
+                //     case "jaipur":
+                //         console.log("The monument in Jaipur is the Jal Mahal.");
+                //         break;
+                //     default:
+                //         console.log("Sorry, no monument information available for that city.");
+                // }
+
+
+
+//-- Write a program to check whether the last digit of a number( entered by user ) is   --//
+//-- divisible by 3 or not.                                                              --//
+
