8d03f8cc983bb39f3c37ab621b51e1ebf143da3e
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-03-05 14:36:06 +0530

    done

--- Work/work1/scripts.js
+++ Work/work1/scripts.js
@@ -1,19 +1,27 @@
-// --A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.     --//
-// --Ask user for their salary and year of service and print the net bonus amount.                          --//
+//-- 1(Q). A company decided to give bonus of 5% to employee if his/her year of service is more than 5 years.     --//
+//--       Ask user for their salary and year of service and print the net bonus amount.                          --//
 
-                    // let salary = 10000;
-                    // let year = 8;
-                    // let bonus = 0;
+                // let salary = 10000;
+                // let year = 8;
+                // let bonus = 0;
 
-                    // if (year > 5) {
-                    //     bonus = salary * 0.05;
-                    // }
+                // if (year > 5) {
+                //     bonus = salary * 0.05;
+                // }
 
-                    // let total = salary + bonus;
-                    // console.log(total);
+                // let total = salary + bonus;
+                // console.log(total);
+
+//-- 1(Q). Write a program to calculate the electricity bill (accept number of unit from user) according to the following criteria :    --//
+//-- Unit                                                        Price                                                                  --//
+//-- First 100 units                                             no charge                                                              --//
+//-- Next 100 units                                              Rs 5 per unit                                                          --//
+//-- After 200 units                                             Rs 10 per unit                                                         --//
+//-- (For example if input unit is 350 than total bill amount is Rs2000)                                                                --//
 
 
-// --Write a program to accept a number from 1 to 7 and display the name of the day like 1 for Sunday , 2 for Monday and so on. --//
+
+//-- 3(Q). Write a program to accept a number from 1 to 7 and display the name of the day like 1 for Sunday , 2 for Monday and so on. --//
 
                 // let dayNumber = parseInt(prompt("Enter a number between 1 and 7: "), 10);
 
@@ -43,11 +51,11 @@
                 //         console.log("Invalid input! Please enter a number between 1 and 7.");
                 // }
 
-//-- Q. Accept any city from the user and display monument of that city. --//
-//--                   City                                 Monument     --//
-//--                   Delhi                               Red Fort      --//
-//--                   Agra                                Taj Mahal     --//
-//--                   Jaipur                              Jal Mahal     --//
+//-- 4(Q). Accept any city from the user and display monument of that city. --//
+//--                      City                                 Monument     --//
+//--                      Delhi                               Red Fort      --//
+//--                      Agra                                Taj Mahal     --//
+//--                      Jaipur                              Jal Mahal     --//
 
                 // let city = prompt("Enter the name of a city (Delhi, Agra, Jaipur): ").toLowerCase();
 
@@ -67,6 +75,24 @@
 
 
 
-//-- Write a program to check whether the last digit of a number( entered by user ) is   --//
-//-- divisible by 3 or not.                                                              --//
+//-- 5(Q). Write a program to check whether the last digit of a number( entered by user ) is   --//
+//--       divisible by 3 or not.                                                              --//
+
+                // let num = parseInt(prompt("Enter a number:"));
+
+                // let lastDigit = num % 10;
+
+                // if (lastDigit % 3 === 0) {
+                //     console.log(`The last digit of ${num} is divisible by 3.`);
+                // } else {
+                //     console.log(`The last digit of ${num} is not divisible by 3.`);
+                // }
+
+
+//-- 6(Q). Write a program to accept the cost price of a bike and display the road tax to be paid according to the following criteria : --//
+
+//--       Cost price (in Rs)                                       Tax                                                                 --//
+//--       > 100000                                                 15 %                                                                --//
+//--       > 50000 and <= 100000                                    10%                                                                 --//
+//--       <= 50000                                                 5%                                                                  --//
 
