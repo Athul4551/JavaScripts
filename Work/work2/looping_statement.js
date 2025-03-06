121df0092a9e51b527e0c46c9fa547658567ac91
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-03-06 16:27:55 +0530

    done

---
+++ Work/work2/looping_statement.js
@@ -0,0 +1,139 @@
+//--Q.(1)print the 1-10 Numbers--//
+
+        // let i=1
+        // while (i<=10){
+        //     console.log(i)
+        //     i++
+        // }
+
+//--Q.(2)print the 1-10 even Numbers--//
+
+        // let i=1
+        // while (i<=10){
+        //     if(i%2==0)
+        //         console.log(i)
+        //     i++ 
+        // }
+
+//--Q.(3)print the 1-10 odd Numbers--//
+
+        // let i=1
+        // while (i<=10){
+        //     if(i%2==1)
+        //         console.log(i)
+        //     i++ 
+        // }
+
+//-- while looping statement--//
+
+        // let i=1
+        // while (i<=10){
+        //     console.log(i)
+        //     i++
+        // }
+
+//-- Do While looping statement--//
+
+        // let i=1
+        // do {
+        //     console.log(i)
+        //     i++
+        // }
+        // while (i<=10)
+
+//-- for looping statement--//
+
+        // for(i=1;1<=10;i++){
+        //     console.log(i)
+        // }
+
+
+//-- (The for in & for of method work in Array) --//
+
+//-- for in method (array)--//
+
+        // let l=[10,20,30,40]
+        // for(i in l){
+        //     console.log(i)
+        // }
+
+//-- for of method (array)--//
+
+        // let l=[10,20,30,40]
+        // for(i of l){
+        //     console.log(i)
+        // }
+
+
+//-- (The for in method work in doc and not work in for of method) --//
+
+//-- for in method (dictionary)--//
+
+        // let a= {name:"abhi",age:21}
+        // for(i in a){
+        //     console.log(i)
+        // }
+
+//-- for of method (dictionary)--//
+
+        // let a= {name:"abhi",age:21}
+        // for(i of a){
+        //     console.log(i)
+        // }
+
+//-- prompt method and value convert to intiger --//
+
+        // let a = parseInt(prompt("Enter The Number:"));
+        // b =prompt("Enter The Number:");
+        // console.log(a);
+        // console.log(typeof a);
+
+//-- prompt Work --//
+
+//-- for method --//
+
+        // let a = parseInt(prompt("Enter The Number:"));
+        // for (let i = 1; i <= 10; i++) {
+        //     console.log(`${i} x ${a} = ${a * i}`);
+        // }
+
+//-- while method --//
+
+        // let a = parseInt(prompt("Enter The Number:"));
+        // let i = 1;
+
+        // while (i <= 10) {
+        //     console.log(`${i} x ${a} = ${a * i}`);
+        //     i++;
+        // }
+
+//-- do while method --//
+
+        // let a = parseInt(prompt("Enter The Number:"));
+        // let i = 1;
+
+        // do {
+        //     console.log(`${i} x ${a} = ${a * i}`);
+        //     i++;
+        // } while (i <= 10);
+
+//-- for in method --//
+
+        // let a = parseInt(prompt("Enter The Number:"));
+        // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+        // for (let i in numbers) {
+        //     console.log(`${numbers[i]} x ${a} = ${a * numbers[i]}`);
+        // }
+
+//-- for of method --//
+
+        // let a = parseInt(prompt("Enter The Number:"));
+        // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        
+        // for (let num of numbers) {
+        //     console.log(`${num} x ${a} = ${a * num}`);
+        // }
+
+
+        
