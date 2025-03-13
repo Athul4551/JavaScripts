c8b319cace51849b9a973f9a0ba710128bb50beb
Abhinavcv2002 <abhinavcv227@gmail.com>
2025-03-13 16:27:06 +0530

    done

---
+++ Work/DOM-in-js/dom1.js
@@ -0,0 +1,34 @@
+
+// let a = document.querySelector("#sample")
+// let b = document.querySelector(".s1")
+// let c = document.querySelectorAll("p")
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//-- css in java scripts --//
+
+        // let a = document.getElementById('sample')
+        // a.setAttribute('style', 'color:red')
+        // a.setAttribute('class', 's1')
+        // console.log(a.getAttribute('style'));
+        // a.classList.add('s2','s3')
+        // a.classList.remove('s1')
+
+//-- variable add method --//
+
+        // let div = document.querySelector('div')
+        // let p = document.createElement('p')
+        // p.innerHTML = 'hello world'
+        // div.appendChild(p)
+
+        // div.removeChild(p)
+
+
+
+// function myfunction(){
+//     let a = window.confirm("Press a button!")
+//     console.log(a);    
+// }
+
+
---
+++ Work/DOM-in-js/dom2.js
@@ -0,0 +1,66 @@
+// const btn = document.querySelector('button')
+// console.log(btn);
+// btn.onclick = function(){
+//     alert('hiiiiiiiiiiiiiii')
+// };
+// btn.onclick = () => alert('hlooooooooo')
+
+// const a = document.querySelector('a');
+// a.onclick = () => alert('heeyyyyyyyy')
+
+//-- MouseEvent example --//
+
+        // const btn = document.querySelector('button');
+        // btn.addEventListener('mouseenter', () => {
+        //     const p = document.querySelector('p')
+        //     p.style.color = 'red'
+
+        // })
+
+        // const btn1 = document.querySelector('button');
+        // btn1.addEventListener('mouseleave', () => {
+        //     const p = document.querySelector('p')
+        //     p.style.color = 'blue'
+
+        // })
+
+//-- KeyboardEvent --//
+
+        // const btn1 = document.querySelector('button');
+        // btn1.addEventListener('keypress', () => {
+        //     const p = document.querySelector('p')
+        //     p.style.color = 'blue'
+
+        // })
+
+
+//-- Timeset in website --//
+
+        //     const btn = document.querySelector("button")
+        //     btn.addEventListener('click', () => {
+        //         console.log('Clicked');
+                
+
+        //     setTimeout(() =>{
+        //         alert("welcome")
+        //     },2000)
+        //     console.log('end');
+        // });
+
+        // setInterval(() => {
+        //     console.log('helo');
+            
+        // },1000);
+
+//-- reversed the number count method (10 to 1) --//
+
+        // const p = document.querySelector("p")
+        // let a = 10;
+        // const x =setInterval(() => {
+        //     a--;
+        //     p.textContent = a;
+        //     if(a === 0){
+        //         clearInterval(x);
+        //     }
+        // },1000)
+
